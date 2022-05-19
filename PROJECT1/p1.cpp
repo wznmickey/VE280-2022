4c3ee7bbe1bb03387e9e751b1b932b572a49fc00
wznmickey <first@wznmickey.com>
2022-05-19 18:08:47 +0800

    lab1 pre

---
+++ PROJECT1/p1.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+void print_prompt( )
+{
+    std::cout
+        << "Please enter the integer and the test number: ";
+}
+
+int main( )
+{
+    // TODO: write your code here
+    return 0;
+}
