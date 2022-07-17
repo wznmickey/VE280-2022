7024e3fd1349396d600892b4fa40ab5ce4857e06
wznmickey <first@wznmickey.com>
2022-07-17 22:05:51 +0800

    p5 init

---
+++ Project 5/Project-5-Related-Files/sam.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include "Dlist.h"
+#include "Instr.h"
+
+int main(int argc, char *argv[])
+{
+    //TODO:write your code here.
+    return 0;
+}
